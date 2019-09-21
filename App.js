daf0ab11915b17092c6464736ce0b9a82a45aa83
Duncan Keith <git@duncankeith.me>
2019-09-21 16:20:49 -0700

    Created a scrollview

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native"
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff"
+  },
+  habitRow: {
+    width: "100%",
+    height: 50,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    padding: 5
+  },
+  habitBox: {
+    flex: 1,
+    backgroundColor: "#00f",
+    alignItems: "flex-start",
+    alignContent: "flex-start"
   }
 })
 
+const HabitRow = ({ title }) => (
+  <View style={style.habitRow}>
+    <View style={style.habitBox}>
+      <Text>{title}</Text>
+    </View>
+  </View>
+)
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    habits = ["Leetcode", "Running", "Meditation"]
     return (
-      <View style={style.container}>
-        <Text>Test</Text>
-      </View>
+      <SafeAreaView style={style.container}>
+        <ScrollView>
+          {habits.map((title, key) => (
+            <HabitRow key={key} title={title} />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
