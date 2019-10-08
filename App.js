c60faeee16b232072109e20846b244fd0bfc9403
Duncan Keith <git@duncankeith.me>
2019-10-07 17:13:09 -0700

    Added statistics screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createAppContainer } from "react-navigation"
 
 import NewHabitScreen from "./NewHabitScreen"
 import HomeScreen from "./HomeScreen"
+import StatisticsScreen from "./StatisticsScreen"
 import { createStackNavigator } from "react-navigation-stack"
 import { createBottomTabNavigator } from "react-navigation-tabs"
 
@@ -18,8 +19,8 @@ const AppNavigator = createBottomTabNavigator({
   Home: {
     screen: HomeStack
   },
-  SecondScreen: {
-    screen: NewHabitScreen
+  Statistics: {
+    screen: StatisticsScreen
   }
 })
 
--- StatisticsScreen.js
+++ StatisticsScreen.js
@@ -1,7 +1,22 @@
 import React from "react"
+import { SafeAreaView, View, StyleSheet } from "react-native"
+
+const style = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#00f" }
+})
 
 class StatisticsScreen extends React.Component {
   constructor(props) {
     super(props)
   }
+
+  render() {
+    return (
+      <SafeAreaView style={style.container}>
+        <View></View>
+      </SafeAreaView>
+    )
+  }
 }
+
+export default StatisticsScreen
