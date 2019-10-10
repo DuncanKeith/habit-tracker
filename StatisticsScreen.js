1bfcfb7317fcc4864388b27d87fd7d46b2c8cf19
Abdullah Abdulqader <UP2LLAA@GMAIL.COM>
2019-10-09 19:58:07 -0700

    Filled This Week stats, but not the circular progress bar.

--- StatisticsScreen.js
+++ StatisticsScreen.js
@@ -3,7 +3,7 @@ import { SafeAreaView, View, StyleSheet, Text } from "react-native"
 
 const style = StyleSheet.create({
   container: { flex: 1, backgroundColor: "#00f" },
-  thisWeeksProgressContainer: { 
+  thisWeekContainer: { 
     flex: 4,
     flexDirection:"row",
     backgroundColor: "#0f0"
@@ -17,16 +17,23 @@ const style = StyleSheet.create({
     flex: 1, 
     justifyContent: "center", 
     alignItems: "center", 
-    backgroundColor: "#444"
+    backgroundColor: "#999"
   }
 })
 
 class StatisticsScreen extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      hoursToday: 0.0,
+      dailyAvg: 0.0,
+      daysLeft: 0.0
+    }
   }
 
   render() {
+    const {hoursToday, dailyAvg, daysLeft} = this.state
     return (
       <SafeAreaView style={style.container}>
         
@@ -34,19 +41,25 @@ class StatisticsScreen extends React.Component {
           <Text>This Week's Progress</Text>
         </View>
 
-        <View style={style.thisWeeksProgressContainer}>
+        <View style={style.thisWeekContainer}>
           <View style={{flex: 2, justifyContent: "center", alignItems: "center", backgroundColor: "#f0f"}}>
               {/* Here goes the circular weekly progress bar */}
           </View>
           <View style={{flex: 1, flexDirection:"column", justifyContent: "center", backgroundColor: "#0ff"}}>
-              <View style={{flex: 1, flexDirection:"column", backgroundColor: "#fff"}}>
+            <View style={{flex: 1, flexDirection:"column", justifyContent: "center", alignItems: "center", backgroundColor: "#fff"}}>
               {/* Here goes the first row of this week's progress stats */}
+              <Text>Today</Text>
+              <Text>{hoursToday} h</Text>
             </View>
-            <View style={{flex: 1, flexDirection:"column", backgroundColor: "#ff0"}}>
+            <View style={{flex: 1, flexDirection:"column", justifyContent: "center", alignItems: "center", backgroundColor: "#ff0"}}>
               {/* Here goes the second row of this week's progress stats */}
+              <Text>Daily Avg.</Text>
+              <Text>{dailyAvg} h</Text>
             </View>
-            <View style={{flex: 1, flexDirection:"column", backgroundColor: "#6f6"}}>
+            <View style={{flex: 1, flexDirection:"column", justifyContent: "center", alignItems: "center", backgroundColor: "#6f6"}}>
               {/* Here goes the third row of this week's progress stats */}
+              <Text>Days Left</Text>
+              <Text>{daysLeft} days</Text>
             </View>
           </View>
         </View>
